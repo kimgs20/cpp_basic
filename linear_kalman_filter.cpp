cf30e41ba16b2929ef194cb3ad557d08af0de112
kgs <kimgeunsukgs@gmail.com>
2022-02-02 20:56:32 +0900

    chapter modify

---
+++ linear_kalman_filter.cpp
@@ -0,0 +1,27 @@
+/*
+
+1. gaussian sampling
+
+2. 2D array
+
+3. prediction-measurment
+
+4. referece position, velocity
+
+*/
+
+
+#include <iostream>
+
+int main() {
+
+    return 0;
+}
+
+float prediction() {
+
+}
+
+float measurement() {
+
+}
