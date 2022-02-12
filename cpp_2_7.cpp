57a50f857d5688dedf09de9ef38cbce3b4e9a687
kgs <kimgeunsukgs@gmail.com>
2022-02-12 22:16:09 +0900

    class, initializer list, static variable, this pointer

--- cpp_2_7.cpp
+++ cpp_2_7.cpp
@@ -1,12 +1,16 @@
 /*
 initializer list
 static member variable
+this pointer
 */
 
 #include <iostream>
 
 class Marine {
     static int total_marine_num;  // for counting num of units
+    // Once static variable is declared, it exists till the program executes. 
+
+    const static int i = 0;
 
     int hp;
     int coord_x, coord_y;
@@ -16,11 +20,12 @@ class Marine {
 
     public:
         Marine();               // default constructor
-        Marine(int x, int y);                           // ?
-        Marine(int x, int y, int default_damage);       // ?
+        Marine(int x, int y);
+        Marine(int x, int y, int default_damage);
 
         int attack();
-        void be_attacked(int damage_earn);
+        // void be_attacked(int damage_earn);
+        Marine& be_attacked(int damage_eran);
         void move(int x, int y);
 
         void show_status();
@@ -28,13 +33,14 @@ class Marine {
         ~Marine() { total_marine_num--; }  // destuctor and decrease count
 };
 
-int Marine::total_marine_num = 0;
+int Marine::total_marine_num = 0;  // initialization of static variable
 
 void Marine::show_total_marine() {
     std::cout << "total number of units: " << total_marine_num << std::endl;
 }
 
 // initializer list
+// create and intialization simultaneously
 Marine::Marine()
     : hp(50), coord_x(0), coord_y(0), default_damage(5), is_dead(false) {
     total_marine_num++;
@@ -61,34 +67,52 @@ void Marine::move(int x, int y) {
 
 int Marine::attack() { return default_damage; }
 
-void Marine::be_attacked(int damage_earn) {
+Marine& Marine::be_attacked(int damage_earn) {
     hp -= damage_earn;
     if (hp <= 0) is_dead = true;
+
+    return *this;
 }
+// same as
+// Marine& Marine::be_attacked(int damage_earn) {
+//     this->hp -= damage_earn;
+//     if (this->hp <= 0) this->is_dead = true;
+
+//     return *this;
+// }
 
 void Marine::show_status() {
     std::cout << "Marine" << std::endl;
     std::cout << "Location: (" << coord_x << ", " << coord_y << ") " << std::endl;
     std::cout << "HP: " << hp << std::endl;
     std::cout << "total number of units: " << total_marine_num << std::endl;
+    std::cout << std::endl;
 }
 
 void create_marine() {
     Marine marine3(10, 10, 4);
-    marine3.show_total_marine();
+    Marine::show_total_marine();
+    // marine3.show_total_marine();
 }
 
 int main() {
     Marine marine1(2, 3, 5);
-    marine1.show_total_marine();
+    Marine::show_total_marine();     // recommended
+    // marine1.show_total_marine();  // it also works
 
     Marine marine2(3, 5, 10);
-    marine2.show_total_marine();
+    Marine::show_total_marine();
+    // marine2.show_total_marine();
 
     create_marine();
 
-    std::cout << std::endl << "marine1 attack marine2" << std::endl;
-    marine2.be_attacked(marine1.attack());
+    // std::cout << std::endl << "marine1 attack marine2" << std::endl;
+    // marine2.be_attacked(marine1.attack());
+
+    std::cout << std::endl << "marine1 attack marine2 twice" << std::endl;
+    marine2.be_attacked(marine1.attack()).be_attacked(marine1.attack());
+
+    std::cout << std::endl;
 
     marine1.show_status();
     marine2.show_status();
