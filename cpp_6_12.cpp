911452c31a7471a201a438057704021f491f0583
kgs <kimgeunsukgs@gmail.com>
2022-03-03 21:44:44 +0900

    template meta programming 4

---
+++ cpp_6_12.cpp
@@ -0,0 +1,66 @@
+/*
+template meta programming (TMP) 4
+*/
+
+#include <iostream>
+
+template <int N>
+struct INT {
+    static const int num = N;
+};
+
+template <typename a, typename b>
+struct add {
+    typedef INT<a::num + b::num> result;
+};
+
+template <typename a, typename b>
+struct divide {
+    typedef INT<a::num / b::num> result;
+};
+
+using one = INT<1>;
+using two = INT<2>;
+using three = INT<3>;
+
+template <typename N, typename d>
+struct check_div {
+    static const bool result = (N::num % d::num == 0) ||
+                                check_div<N, typename add<d, one>::result>::result;
+};
+
+template <typename N>
+struct _is_prime {
+    static const bool result =! check_div<N, two>::result;
+};
+
+template <>
+struct _is_prime<two> {
+    static const bool result = true;
+};
+
+template <>
+struct _is_prime<three> {
+    static const bool result = true;
+};
+
+template <typename N>
+struct check_div<N, typename divide<N, two>::result> {
+    static const bool result = (N::num % (N::num / 2) == 0);
+};
+
+template <int N>
+struct is_prime {
+    static const bool result = _is_prime<INT<N>>::result;
+};
+
+
+int main() {
+    std::cout << std::boolalpha;
+    std::cout << "is 2 prime?: " << is_prime<2>::result << std::endl;
+    std::cout << "is 10 prime?: " << is_prime<10>::result << std::endl;
+    std::cout << "is 11 prime?: " << is_prime<11>::result << std::endl;
+    std::cout << "is 61 prime?: " << is_prime<61>::result << std::endl;
+
+    return 0;
+}
