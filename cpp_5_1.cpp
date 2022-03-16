10b916f1704d2a1a8e1380572b4b0c36125cf9d3
kgs <kimgeunsukgs@gmail.com>
2022-03-16 20:54:31 +0900

    input output stream

---
+++ cpp_5_1.cpp
@@ -0,0 +1,42 @@
+/*
+input, output stream
+
+fstream, ofstream, ifstram, iostream, istream, ostream, ios, ios_base
+
+ios initialize stream buffer(temporarily store data)
+
+flag of ios class
+- goodbit
+- badbit
+- failbit
+- eofbit
+*/
+
+#include <iostream>
+#include <string>
+
+int main() {
+    int t;
+    while (true) {
+        std::cin >> t;
+        std::cout << "input: " << t << std::endl;
+        if (std::cin.fail()) {  // true of failbit or badbit: return true
+            std::cout << "input int data" << std::endl;
+            std::cin.clear();  // reinitialize flags
+            std::cin.ignore(100, '\n');  // ignore until new line character (max 100 character)
+        }
+        if (t == 1) break;
+    }
+}
+/*
+4
+input: 4
+5
+input: 5
+s
+input: 0
+input int data
+q
+input: 0
+input int data
+*/
