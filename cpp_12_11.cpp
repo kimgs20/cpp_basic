6a9ddaf3089266d8a8a154523c6eb6101a54afe1
kgs <kimgeunsukgs@gmail.com>
2022-04-10 23:31:36 +0900

    thread - promise-futurn

--- cpp_12_11.cpp
+++ cpp_12_11.cpp
@@ -2,7 +2,8 @@
 thread - promise-future
 */
 
-#include <condition_variable>
+// 1. condition variable
+/* #include <condition_variable>
 #include <iostream>
 #include <mutex>
 #include <string>
@@ -31,6 +32,43 @@ int main() {
 
     std::cout << "return data: " << info << std::endl;
 
+    t.join();
+
+    return 0;
+} */
+
+// 2. promise - future (much clean and easy than cv)
+#include <exception>
+#include <future>
+#include <iostream>
+#include <string>
+#include <thread>
+
+using std::string;
+
+void worker(std::promise<string>* p) {
+    try {
+        throw std::runtime_error("some error");
+    } catch (...) {
+        // should deliver exception_ptr to set_exception
+        p->set_exception(std::current_exception());
+    }
+}
+
+int main() {
+    std::promise<string> p;
+
+    std::future<string> data = p.get_future();
+
+    std::thread t(worker, &p);
+
+    data.wait();
+
+    try {
+        data.get();
+    } catch (const std::exception& e) {
+        std::cout << "exception: " << e.what() << std::endl;
+    }
     t.join();
 
     return 0;
