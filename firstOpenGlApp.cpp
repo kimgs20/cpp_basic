ad7804a206c7a12156c6bf13a1e9214d61970796
kgs <kimgeunsukgs@gmail.com>
2022-04-24 20:33:53 +0900

    openGL

---
+++ firstOpenGlApp.cpp
@@ -0,0 +1,34 @@
+/*
+$ sudo apt update
+$ sudo apt-get install freeglut3-dev
+
+check /usr/incldue/GL/freeglut.h or glut.h
+*/
+
+#include "GL/glut.h"
+
+void dispalyMe(void) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello OpenGL!");
+    glutDisplayFunc(dispalyMe);
+    glutMainLoop();
+    return 0;
+}
+/*
+$ g++ firstOpenGlApp.cpp -o firstOpenGlApp -lglut -lGLU -lGL
+$ ./firstOpenGlApp
+ */
