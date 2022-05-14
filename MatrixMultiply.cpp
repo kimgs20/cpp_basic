798691b149587a7fa09dd6f3f70b259578df038e
kgs <kimgeunsukgs@gmail.com>
2022-05-14 22:49:10 +0900

    dumb calculation

--- MatrixMultiply.cpp
+++ MatrixMultiply.cpp
@@ -20,15 +20,31 @@ ans = [47 52 57
 
 #include <iostream>
 
-void matmul(int (*aMat)[2]) {
-     // 이게... 맞나...?
-     // https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=tipsware&logNo=221329432324
-     std::cout << (*(aMat))[0] << std::endl;
-     std::cout << (*(aMat))[1] << std::endl;
-     std::cout << (*(aMat + 1))[0] << std::endl;
-     std::cout << (*(aMat + 1))[1] << std::endl;
-     std::cout << (*(aMat + 2))[0] << std::endl;
-     std::cout << (*(aMat + 2))[1] << std::endl;
+// 다차원 배열 인자로 사용하기
+// https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=tipsware&logNo=221329432324
+void matmul(int (*aMat)[2], int (*bMat)[3], int (*ans)[3]) {
+
+     // highschool type calculation
+     (*(ans))[0] = (*(aMat))[0] * (*(bMat))[0] + (*(aMat))[1] * (*(bMat + 1))[0];
+     (*(ans))[1] = (*(aMat))[0] * (*(bMat))[1] + (*(aMat))[1] * (*(bMat + 1))[1];
+     (*(ans))[2] = (*(aMat))[0] * (*(bMat))[2] + (*(aMat))[1] * (*(bMat + 1))[2];
+
+     (*(ans + 1))[0] = (*(aMat + 1))[0] * (*(bMat))[0] + (*(aMat + 1))[1] * (*(bMat + 1))[0];
+     (*(ans + 1))[1] = (*(aMat + 1))[0] * (*(bMat))[1] + (*(aMat + 1))[1] * (*(bMat + 1))[1];
+     (*(ans + 1))[2] = (*(aMat + 1))[0] * (*(bMat))[2] + (*(aMat + 1))[1] * (*(bMat + 1))[2];
+
+     (*(ans + 2))[0] = (*(aMat + 2))[0] * (*(bMat))[0] + (*(aMat + 2))[1] * (*(bMat + 1))[0];
+     (*(ans + 2))[1] = (*(aMat + 2))[0] * (*(bMat))[1] + (*(aMat + 2))[1] * (*(bMat + 1))[1];
+     (*(ans + 2))[2] = (*(aMat + 2))[0] * (*(bMat))[2] + (*(aMat + 2))[1] * (*(bMat + 1))[2];
+
+     // 보통 for loop 세개로 구현함
+     // for (int i = 0; i < n-1; i++) {
+     //      for (int j = 0; j < n-1; j++) {
+     //           for (int k = 0; k < n-1; k++) {
+     //                c_ij = c_ij + a_ik * b_kj;
+     //           }
+     //      }
+     // }
 }
 
 int main() {
@@ -49,19 +65,19 @@ int main() {
      for (int i = 0; i < 3; i++) {
           for (int j = 0; j < 3; j++) {
                ans[i][j] = 0;
-               // std::cout << ans[i][j] << " ";
           }
-          // std::cout << std::endl;
      }
 
-     // std::cout << aMat << std::endl;
+     // 각 array의 첫 번째 주소 전달
+     matmul(aMat, bMat, ans);
 
-     matmul(aMat);
+     std::cout << "after matmul in main" << std::endl;
+     for (int i = 0; i < 3; i++) {
+          for (int j = 0; j < 3; j++) {
+               std::cout << ans[i][j] << " ";
+          }
+          std::cout << std::endl;
+     }
 
      return 0;
 }
-/* 
-TODO
-
-row, col을 구해서 print 하는 함수
- */
